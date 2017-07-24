73b1488b75b1763bba26063172fe8752c66d1de7
nicolas <nicolasbloj@gmail.com>
2017-07-24 09:51:08 -0300

    top-bar and main-content into shell

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
+import { MainContentComponent } from './shell/main-content/main-content.component';
+import { TopBarComponent } from './shell/top-bar/top-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ShellComponent]
+  declarations: [
+    ShellComponent, 
+    MainContentComponent, // no exportamos, se usa solo dentro de shell.
+    TopBarComponent // no exportamos, se usa solo dentro de shell.
+  ]
 })
 export class CoreModule { }
