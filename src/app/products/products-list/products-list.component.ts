12f76e641c4d7ce2ec4ad795f827021125442f94
nicolas <nicolasbloj@gmail.com>
2017-07-26 01:02:04 -0300

    emit produts from products-list and emit product from product-add

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { ProductService } from '../_service/product.service';
 import { Product } from '../../_model/product.model';
@@ -12,12 +12,17 @@ export class ProductsListComponent implements OnInit {
 
   products: Product[];
 
+  @Output()
+  emitter = new EventEmitter<Product[]>();
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-
-    this._productService.list().subscribe(_data => { this.products = _data; });
-
+    this._productService.list().subscribe(
+      _data => {
+        this.products = _data; this.emitter.emit(this.products);
+      });
   }
+
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../_model/product.model';
+
 @Component({
   selector: 'nab-products',
   templateUrl: './products.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products: Product[];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addProduct(aproduct: Product): void {
+    this.products.push(aproduct);
+  }
+
+  setProducts(products: Product[]): void {
+    this.products = products;
   }
 
 }
