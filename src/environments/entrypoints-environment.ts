e425b6f4e7c7556e1a83c698835931b0a3432a92
nicolas <nicolasbloj@gmail.com>
2017-08-01 18:55:45 -0300

    some bootstrap

--- src/environments/entrypoints-environment.ts
+++ src/environments/entrypoints-environment.ts
@@ -1,11 +1,11 @@
 export const server = {
-    pos_endpoint: 'http://localhost:8080/pos-endpoint'
+    pos_endpoint: 'http://172.20.1.173:8080/pos-endpoint'
 };
 
 export const product_resource = {
     list : '/product/list',
     addOrUpdate : '/product/addOrUpdate'
-    
+
 };
 
 export const product_api = {
