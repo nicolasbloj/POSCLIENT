d48e5e6739a6e5e7acbc629d2b4d926e65b80e19
nicolas <nicolasbloj@gmail.com>
2017-09-06 17:03:40 -0300

    CryptoJS

--- src/app/login/_service/login.service.ts
+++ src/app/login/_service/login.service.ts
@@ -2,6 +2,8 @@ import { UserService } from '../../user/_service/user.service';
 import { Injectable } from '@angular/core';
 import { User } from '../../_model/person/user.model';
 
+import * as CryptoJS from 'crypto-js';
+
 @Injectable()
 export class LoginService {
 
@@ -10,6 +12,15 @@ export class LoginService {
   public login(_username: string, _password: string): User {
     const user: User = this._userService.getUser(_username.toLocaleLowerCase(), _password);
     if (user != null) {
+      // Encrypt the Passwort with Base64
+      const key = CryptoJS.enc.Base64.parse('#base64Key#');
+      const iv = CryptoJS.enc.Base64.parse('#base64IV#');
+
+      // Impementing the Key and IV and encrypt the password
+      const encrypted = CryptoJS.AES.encrypt(user.password, key, { iv: iv });
+
+      user.password = encrypted.toString();
+
       localStorage.setItem('user', JSON.stringify(user));
       return user;
     }
